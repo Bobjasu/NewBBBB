2dbef403c7b4f87cb428ec97495fc0564a4ea68e
Bobjasu <utsab12.bcet@gmail.com>
2023-06-07 11:01:14 +0530

    commited

---
+++ src/test/java/practice/SampleTest.java
@@ -0,0 +1,21 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+    @Test(priority = 1)
+	public void create()
+	{
+		System.out.println("created");
+	}
+	@Test(priority = 2,invocationCount = 3,dependsOnMethods = "create")
+	public void modified()
+	{
+		System.out.println("modified");
+	}
+	@Test(priority = 3)
+	public void deleted()
+	{
+		System.out.println("deleted");
+	}
+}
